8adef2e769393cdaead776488cf59f88fd67723f
Earlopain <earlopain@c5h8no4na.net>
2019-10-22 21:20:16 +0200

    Show select properties of element

--- preloadSelect.js
+++ preloadSelect.js
@@ -32,7 +32,33 @@ ipcRenderer.on('messagefromcomic', (event, outerHTML, computedStyle, baseURL, cs
 
     template.content.firstChild.id = "copiedelement";
     template.content.firstChild.style.cssText = computedStyle;
+    let devtools = document.getElementById("devtools");
+    devtools.innerHTML = "";
+
+
+    const ignoreAttributes = ["style", "id", ";", "border"];
+    const definitelyURLS = ["src", "href"];
+
+    for (const attr of template.content.firstChild.attributes) {
+        if(ignoreAttributes.includes(attr.name)){
+            continue;
+        }
+        if(definitelyURLS.includes(attr.name)){
+            debugger;
+            attr.value = fixURL(attr.value, baseURL);
+        }
+        let text = document.createTextNode(innerHTML = attr.name + ":" + attr.value);
+        devtools.appendChild(text);
+        devtools.appendChild(document.createElement("br"));
+    }
 
     copied = document.getElementById("copiedelementcontainer");
     copied.innerHTML = template.content.firstChild.outerHTML;
 });
+
+function fixURL(url, baseURL){
+    if(url.startsWith("http")){
+        return url;
+    }
+    return baseURL + url;
+}
