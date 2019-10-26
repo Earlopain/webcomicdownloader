514f7a7ddec75c548e7d34460a18a45ace9b7b64
Earlopain <earlopain@c5h8no4na.net>
2019-10-26 14:51:41 +0200

    Construct object with almost all info needed to dl a comic

--- preloadSelect.js
+++ preloadSelect.js
@@ -3,28 +3,67 @@
 
 const { remote, ipcRenderer } = require("electron");
 let sendToView = remote.getGlobal("sendToView");
-let i;
+let getView = remote.getGlobal("getView")
+let statusMessages = ["Please navigate to the page with the first page and hit select",
+    "Please naviagte to the page with the most recent page and hit select<br>Examples for valid urls: <br>https://www.xkcd.com/<br>http://www.minnasundberg.fi/comic/recent.php<br>Invalid urls:<br>https://www.xkcd.com/2220/<br>http://www.minnasundberg.fi/comic/page556.php",
+    "Please click on the element which redirects you to the previous page",
+    "Please click on the element which redirects you to the next page",
+    "Please click on the element which contains the image"];
+let comicResult = {};
+let currentStatus = 0;
 window.addEventListener("DOMContentLoaded", () => {
+    setStatus(0);
     document.getElementById("getparentbutton").onclick = () => {
         sendToView("comic", "getparent");
     }
     document.getElementById("getchildrenbutton").onclick = () => {
         sendToView("comic", "getchildren");
     }
+    document.getElementById("selectelement").onclick = () => {
+        let selector = document.querySelector("#copiedelementcontainer > div > div:nth-child(2)");
+        switch (currentStatus) {
+            case 0:
+                comicResult.firstPage = getView("comic").webContents.getURL();
+                break;
+            case 1:
+                comicResult.lastPage = getView("comic").webContents.getURL();
+                break;
+            case 2:
+                comicResult.prevPage = decodeEntities(selector.innerHTML);
+                break;
+            case 3:
+                comicResult.nextPage = decodeEntities(selector.innerHTML);
+                break;
+            case 4:
+                comicResult.image = decodeEntities(selector.innerHTML);
+                break;
+
+            default:
+                console.log(comicResult);
+                break;
+        }
+        currentStatus++;
+        setStatus(currentStatus);
+    }
 });
 
-ipcRenderer.on("showchildren", (event, outerHTMLArray, computedStyleArray, cssSelectorArray, baseURL) => {
+function setStatus(id) {
+    document.getElementById("statusmessage").innerHTML = statusMessages[id];
+}
+
+ipcRenderer.on("showchildren", (event, outerHTMLArray, computedStyleArray, cssSelectorArray, baseURL, url) => {
+    currentComicUrl = url;
     setBase(baseURL);
     resetCopiedElements();
 
-    for(let i = 0; i < outerHTMLArray.length; i++){
+    for (let i = 0; i < outerHTMLArray.length; i++) {
         const outerHTML = outerHTMLArray[i];
         const computedStyle = computedStyleArray[i];
         const cssSelector = cssSelectorArray[i];
         const element = createElement(outerHTML, computedStyle);
         const button = document.createElement("button");
         button.classList.add("padding");
-        button.onclick =  () => {
+        button.onclick = () => {
             sendToView("comic", "selectchild", i);
             setBase(baseURL);
             resetCopiedElements();
@@ -35,11 +74,12 @@ ipcRenderer.on("showchildren", (event, outerHTMLArray, computedStyleArray, cssSe
     }
 });
 
-ipcRenderer.on("showsingleelement", (event, outerHTML, computedStyle, cssSelector, baseURL) => {
+ipcRenderer.on("showsingleelement", (event, outerHTML, computedStyle, cssSelector, baseURL, url) => {
+    currentComicUrl = url;
     setBase(baseURL);
     resetCopiedElements();
     const element = createElement(outerHTML, computedStyle);
-    
+
     addCopiedElement(element, cssSelector, baseURL);
 });
 
@@ -47,7 +87,7 @@ function resetCopiedElements() {
     document.getElementById("copiedelementcontainer").innerHTML = "";
 }
 
-function addCopiedElement(element, cssSelector, baseURL, button){
+function addCopiedElement(element, cssSelector, baseURL, button) {
     let divContainer = document.createElement("div");
     let copiedElement = document.createElement("div");
     copiedElement.class = "elementcontainer float";
@@ -75,7 +115,7 @@ function addCopiedElement(element, cssSelector, baseURL, button){
         devtools.appendChild(document.createElement("br"));
     }
     divContainer.appendChild(devtools);
-    if(button !== undefined){
+    if (button !== undefined) {
         divContainer.appendChild(button);
     }
     document.getElementById("copiedelementcontainer").appendChild(divContainer);
@@ -111,8 +151,28 @@ function fixURL(url, baseURL) {
     if (url.startsWith("http")) {
         return url;
     }
-    if(url.startsWith("//")){
+    if (url.startsWith("//")) {
         return baseURL.split("://")[0] + ":" + url;
     }
     return baseURL + url;
 }
+
+let decodeEntities = (function () {
+    // this prevents any overhead from creating the object each time
+    let element = document.createElement('div');
+
+    function decodeHTMLEntities(str) {
+        if (str && typeof str === 'string') {
+            // strip script/html tags
+            str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+            str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+            element.innerHTML = str;
+            str = element.textContent;
+            element.textContent = '';
+        }
+
+        return str;
+    }
+
+    return decodeHTMLEntities;
+})();
